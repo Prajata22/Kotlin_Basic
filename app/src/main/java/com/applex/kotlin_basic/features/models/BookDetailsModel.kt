65e85014a6fdfc646aac134be2d0f8c32a25ba01
Prajata Samanta <prajatasamanta2204@gmail.com>
2021-06-22 14:12:14 +0530

    Prajata

--- app/src/main/java/com/applex/kotlin_basic/features/models/BookDetailsModel.kt
+++ app/src/main/java/com/applex/kotlin_basic/features/models/BookDetailsModel.kt
@@ -1,12 +1,29 @@
 package com.applex.kotlin_basic.features.models
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by Prajata on 25 May 2021
  */
 data class BookDetailsModel(
+    @Expose
+    @SerializedName("book_name")
     val book_name: String?,
+
+    @Expose
+    @SerializedName("author")
     val author: String?,
+
+    @Expose
+    @SerializedName("category")
     val category: String?,
+
+    @Expose
+    @SerializedName("publish_date")
     val publish_date: String?,
+
+    @Expose
+    @SerializedName("page_count")
     val page_count: Int?
 )
--- app/src/main/java/com/applex/kotlin_basic/features/models/BooksListModel.kt
+++ app/src/main/java/com/applex/kotlin_basic/features/models/BooksListModel.kt
@@ -1,6 +1,13 @@
 package com.applex.kotlin_basic.features.models
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by Prajata on 25 May 2021
  */
-data class BooksListModel(val data: ArrayList<BookDetailsModel>)
+data class BooksListModel(
+    @Expose
+    @SerializedName("data")
+    val data: ArrayList<BookDetailsModel>
+)
